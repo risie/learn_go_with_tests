4b549e57242d9cf8bcce709e12eb8700f92a1ecc
Jimmie Rissanen <jimmieriss@gmail.com>
2024-06-02 08:25:03 +0200

    feat: take a name argument and show dynamic output
    refactor test with assertion helper
    
    format the code to go standard

--- hello.go
+++ hello.go
@@ -1,13 +1,16 @@
-package main 
+package main
 
 import "fmt"
 
 const englishHelloPrefix = "Hello"
 
-func Hello (name string) string {
-  return fmt.Sprintf("%s, %s!",englishHelloPrefix, name)
+func Hello(name string) string {
+	if name == "" {
+		name = "world"
+	}
+	return fmt.Sprintf("%s, %s!", englishHelloPrefix, name)
 }
 
 func main() {
-  fmt.Println(Hello( "world"))
+	fmt.Println(Hello("world"))
 }
--- hello_test.go
+++ hello_test.go
@@ -2,21 +2,28 @@ package main
 
 import "testing"
 
-func TestHello(t*testing.T) {
-  t.Run("saying hello to people", func(t *testing.T){
-  actual := Hello("1")
-  expected := "Hello, 1!"
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		actual := Hello("1")
+		expected := "Hello, 1!"
 
-  if expected != actual{
-    t.Errorf("expected %q but got %q", expected, actual)
-  }
-  })
-  t.Run("default to 'world' when an empty string is supplied", func(t *testing.T){
-    actual := Hello("")
-    expected := "Hello, world!"
+		if expected != actual {
+			assertCorrectMessage(t, actual, expected)
+		}
+	})
+	t.Run("default to 'world' when an empty string is supplied", func(t *testing.T) {
+		actual := Hello("")
+		expected := "Hello, world!"
 
-    if expected != actual{
-      t.Errorf("expected %q but got %q", expected, actual)
-    }
-  })
+		if expected != actual {
+			assertCorrectMessage(t, actual, expected)
+		}
+	})
+}
+
+func assertCorrectMessage(t testing.TB, actual, expected string) {
+	t.Helper()
+	if expected != actual {
+		t.Errorf("expected %q but got %q", expected, actual)
+	}
 }
