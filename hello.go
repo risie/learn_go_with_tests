294e512824bb62b2ec5b32dfd155b099b3dceaeb
Jimmie Rissanen <jimmieriss@gmail.com>
2024-06-02 21:00:41 +0200

    feat: add language choice
    refactor test with a name builder
    
    add test for French
    
    add French as a hand language
    
    add test for defaulting to english
    
    add test case to support Swedish

--- hello.go
+++ hello.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello"
+const (
+	englishHelloPrefix = "Hello"
+	spanishHelloPrefix = "Hola"
+	frenchHelloPrefix  = "Bonjour"
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
+
+	if language == "Spanish" {
+		return fmt.Sprintf("%s, %s!", spanishHelloPrefix, name)
+	}
+
+	if language == "French" {
+		return fmt.Sprintf("%s, %s!", frenchHelloPrefix, name)
+	}
+
 	return fmt.Sprintf("%s, %s!", englishHelloPrefix, name)
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "Spanish"))
 }
--- hello_test.go
+++ hello_test.go
@@ -1,19 +1,74 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHello(t *testing.T) {
+	name := "name"
+	englishGreeting := "Hello"
+	spanishGreeting := "Hola"
+	frenchGreeting := "Bonjour"
+	swedishGreeting := "Hej"
+
 	t.Run("saying hello to people", func(t *testing.T) {
-		actual := Hello("1")
-		expected := "Hello, 1!"
+		actual := Hello(name, "")
+		expected := buildName(t, englishGreeting, name)
 
 		if expected != actual {
 			assertCorrectMessage(t, actual, expected)
 		}
 	})
+
 	t.Run("default to 'world' when an empty string is supplied", func(t *testing.T) {
-		actual := Hello("")
-		expected := "Hello, world!"
+		actual := Hello("", "")
+		expected := buildName(t, englishGreeting, "world")
+
+		if expected != actual {
+			assertCorrectMessage(t, actual, expected)
+		}
+	})
+
+	t.Run("should default to english if language is empty", func(t *testing.T) {
+		actual := Hello(name, "")
+		expected := buildName(t, englishGreeting, name)
+
+		if expected != actual {
+			assertCorrectMessage(t, actual, expected)
+		}
+	})
+
+	t.Run("should default to english if a unsupported language is entered", func(t *testing.T) {
+		actual := Hello(name, "Swedish")
+		expected := buildName(t, englishGreeting, name)
+
+		if expected != actual {
+			assertCorrectMessage(t, actual, expected)
+		}
+	})
+
+	t.Run("should return a greeting in Spanish", func(t *testing.T) {
+		actual := Hello(name, "Spanish")
+		expected := buildName(t, spanishGreeting, name)
+
+		if expected != actual {
+			assertCorrectMessage(t, actual, expected)
+		}
+	})
+
+	t.Run("should return a greeting in French", func(t *testing.T) {
+		actual := Hello(name, "French")
+		expected := buildName(t, frenchGreeting, name)
+
+		if expected != actual {
+			assertCorrectMessage(t, actual, expected)
+		}
+	})
+
+	t.Run("should return a greeting in Swedish", func(t *testing.T) {
+		actual := Hello(name, "Swedish")
+		expected := buildName(t, swedishGreeting, name)
 
 		if expected != actual {
 			assertCorrectMessage(t, actual, expected)
@@ -26,4 +81,9 @@ func assertCorrectMessage(t testing.TB, actual, expected string) {
 	if expected != actual {
 		t.Errorf("expected %q but got %q", expected, actual)
 	}
+}
+
+func buildName(t testing.TB, greeting string, name string) string {
+	t.Helper()
+	return fmt.Sprintf("%s, %s!", greeting, name)
 }
