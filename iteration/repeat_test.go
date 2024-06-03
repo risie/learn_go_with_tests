ff82f735b66e470c50f8d558c5d3ad0afb9f8e08
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-06-03 20:44:16 +0200

    test(repeat_test): add times arg to Repeat

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,16 +3,18 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	actual := Repeat("a")
-	expected := "aaaaa"
+	t.Run("should repeat the character 5 times", func(t *testing.T) {
+		actual := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if actual != expected {
-		t.Errorf("expected '%s' but got '%s'", expected, actual)
-	}
+		if actual != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, actual)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
