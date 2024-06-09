8a49503f01a43c78ec8a5288c8c3770fabe01b9f
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-06-09 11:06:40 +0200

    test(Sum): add assertion helper and use slice over array

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -4,30 +4,32 @@ import "testing"
 
 func TestSum(t *testing.T) {
 	t.Run("should sum up an array of 5 integers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		actual := Sum(numbers)
 		expected := 15
-
-		if actual != expected {
-			t.Errorf("Expected %d but got %d given, %v", expected, actual, numbers)
-		}
+		assertCorrectSuM(t, actual, expected, numbers)
 	})
 	t.Run("should sum up an array of any size", func(t *testing.T) {
 		numbers := []int{1, 2}
 
 		actual := Sum(numbers)
 		expected := 3
-
-		if actual != expected {
-			t.Errorf("Expected %d but got %d given, %v", expected, actual, numbers)
-		}
+		assertCorrectSuM(t, actual, expected, numbers)
 	})
 }
 
 func BenchmarkSum(b *testing.B) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 	for i := 0; i < b.N; i++ {
 		Sum(numbers)
+	}
+}
+
+func assertCorrectSuM(t testing.TB, actual, expected int, numbers []int) {
+	t.Helper()
+
+	if actual != expected {
+		t.Errorf("Expected %d but got %d given, %v", expected, actual, numbers)
 	}
 }
