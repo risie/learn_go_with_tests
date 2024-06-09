24045b2de4a39179b6bd5c7fe7efe5f2f856c4ac
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2024-06-09 11:02:00 +0200

    test(Sum): add test for dynamic input lenght

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -3,14 +3,26 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("should sum up an array of 5 integers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	actual := Sum(numbers)
-	expected := 15
+		actual := Sum(numbers)
+		expected := 15
 
-	if actual != expected {
-		t.Errorf("Expected %d but got %d given, %v", expected, actual, numbers)
-	}
+		if actual != expected {
+			t.Errorf("Expected %d but got %d given, %v", expected, actual, numbers)
+		}
+	})
+	t.Run("should sum up an array of any size", func(t *testing.T) {
+		numbers := []int{1, 2}
+
+		actual := Sum(numbers)
+		expected := 3
+
+		if actual != expected {
+			t.Errorf("Expected %d but got %d given, %v", expected, actual, numbers)
+		}
+	})
 }
 
 func BenchmarkSum(b *testing.B) {
