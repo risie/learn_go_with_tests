b38721051fc16e49915c1b6337d816fe083b9dec
Jimmie Rissanen <71719721+Jimmie-Rissanen@users.noreply.github.com>
2025-02-05 08:39:59 +0100

    refactor(arrays): improve SumAll

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -9,6 +9,11 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	return []int{}
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
 
 }
